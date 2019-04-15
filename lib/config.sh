0a9ddc638e08c596a1dbd96ffa042b6791274b7a
Leonardo Lucena <leonardo.lucena@ifrn.edu.br>
2019-04-15 13:23:01 -0300

    Update config.sh

--- lib/config.sh
+++ lib/config.sh
@@ -1,2 +1,4 @@
 #!/bin/bash
 echo 'alias potigol="java -jar /workspace/gitpod/lib/potigol.jar 2> /dev/null"' | tee -a /home/gitpod/.bashrc
+echo 'alias potigol2scala="/workspace/gitpod/lib/potigol2scala.sh' | tee -a /home/gitpod/.bashrc
+
