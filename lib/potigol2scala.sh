897d0f12e324e7579d7cccdfc6f39199ade508c6
Leonardo Lucena <leonardo.lucena@ifrn.edu.br>
2019-04-15 13:34:54 -0300

    Update potigol2scala.sh

--- lib/potigol2scala.sh
+++ lib/potigol2scala.sh
@@ -7,4 +7,4 @@ cat potigolutil.scala >> $1.scala
 echo "object Main extends App{" >> $1.scala
 potigol -d $1 | tail -n +6 >> $1.scala
 echo "}" >> $1.scala
-cat $1.scala | xclip -selection c
+echo O arquivo $1.scala foi criado
