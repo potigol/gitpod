cf79f0863f9d00e1caa9265b4c7485429e6fffaf
Leonardo Lucena <leonardo.lucena@escolar.ifrn.edu.br>
2021-04-14 18:53:07 +0000

    bashrc configuration

--- lib/config.sh
+++ lib/config.sh
@@ -2,4 +2,4 @@
 echo 'alias potigol="java -jar /workspace/gitpod/lib/potigol.jar 2> /dev/null"' | tee -a /home/gitpod/.bashrc
 echo 'alias potigol2scala="/workspace/gitpod/lib/potigol2scala.sh"' | tee -a /home/gitpod/.bashrc
 chmod +x /workspace/gitpod/lib/potigol2scala.sh
-
+source ~/.bashrc
